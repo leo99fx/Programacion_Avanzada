31b4767622616eeb7f7956a786b11d3635cda5e0
leonel <leonel@Leonel-PC>
2024-04-25 13:34:51 -0300

    Ordenamiento vectores

--- Guia_PAvanzada/src/Ordenamiento/Ordenamiento.java
+++ Guia_PAvanzada/src/Ordenamiento/Ordenamiento.java
@@ -6,17 +6,31 @@ public class Ordenamiento {
 
 		int[] vec = { 99, 156485, 1258, 18, 22, 38, 1, 7, 2, 3 };
 
+		System.out.print("\nVector original: ");
 		for (int num : vec) {
 			System.out.print(num + " ");
 		}
 
 		System.out.print("\nOrdenamiento por Shell: ");
 		OrdenamientoShell(vec);
-		System.out.print("\n");
+
 		for (int num : vec) {
 			System.out.print(num + " ");
 		}
 
+	}
+
+	private static void ordenarPorSeleccion(int[] datos) {
+		int n = datos.length;
+		for (int i = 0; i < (n - 1); i++) {
+			int menor = i;
+			for (int j = i + 1; j < n; j++)
+				if (datos[j] < datos[menor])
+					menor = j;
+			int swap = datos[i];
+			datos[i] = datos[menor];
+			datos[menor] = swap;
+		}
 	}
 
 	public static void OrdenamientoShell(int[] vec) {
@@ -47,6 +61,10 @@ public class Ordenamiento {
 
 			h = (h - 1) / 3;
 		}
+
+	}
+
+	public static void OrdenarQuickSort(int[] vec) {
 
 	}
 
