1591fec4774b1abeab6228f802907b7818254b5f
leonel <leonel@Leonel-PC>
2024-04-27 16:25:18 -0300

    format

--- Guia_PAvanzada/src/Monticulo/Main.java
+++ Guia_PAvanzada/src/Monticulo/Main.java
@@ -1,17 +1,17 @@
 package Monticulo;
 
 public class Main {
-    public static void main(String[] args) {
-        Monticulo monticulo = new Monticulo(10);
-        monticulo.insertar(10);
-        monticulo.insertar(20);
-        monticulo.insertar(15);
-        monticulo.insertar(30);
-        monticulo.insertar(25);
+	public static void main(String[] args) {
+		Monticulo monticulo = new Monticulo(10);
+		monticulo.insertar(10);
+		monticulo.insertar(20);
+		monticulo.insertar(15);
+		monticulo.insertar(30);
+		monticulo.insertar(25);
 
-        monticulo.mostrarMonticulo();
+		monticulo.mostrarMonticulo();
 
-        System.out.println("Eliminando el valor máximo: " + monticulo.eliminar());
-        monticulo.mostrarMonticulo();
-    }
+		System.out.println("Eliminando el valor máximo: " + monticulo.eliminar());
+		monticulo.mostrarMonticulo();
+	}
 }
