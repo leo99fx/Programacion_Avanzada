d59cf13b338bfa3fe5f53d20b08cd5805d5fecad
leonel <leonel@Leonel-PC>
2024-05-10 10:20:57 -0300

    P_Dinamica - Mina de Oro

--- Guia_PAvanzada/src/Prog_Dinamica/Programacion_Dinamica.java
+++ Guia_PAvanzada/src/Prog_Dinamica/Programacion_Dinamica.java
@@ -47,15 +47,57 @@ public class Programacion_Dinamica {
 
 		return anterior > anteriorAnterior ? anterior : anteriorAnterior;
 	}
-	
+
 	// LA MINA DE ORO
-	
-	public static int minaDeOro(int[][]mina) {
-		int res=0;
-		
-		
-		
-		
+
+	public static void imprimirMatriz(int[][] matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int minaDeOro(int[][] mina) {
+		int res = 0;
+
+		int fila = mina.length;
+		int columna = mina[0].length;
+
+		for (int i = 1; i < fila; i++) {
+			for (int j = 0; j < columna; j++) {
+				if (i - 1 >= 0 && j - 1 >= 0 && j + 1 < columna) {
+
+					int mayorIzq = mina[i - 1][j] > mina[i - 1][j - 1] ? mina[i - 1][j] : mina[i - 1][j - 1];
+					int mayorDer = mina[i - 1][j] > mina[i - 1][j + 1] ? mina[i - 1][j] : mina[i - 1][j + 1];
+
+					int mayor = mina[i][j] + (mayorIzq > mayorDer ? mayorIzq : mayorDer);
+
+					mina[i][j] = mayor;
+
+				} else if (i - 1 >= 0 && j == 0) {
+
+					int mayor = mina[i][j]
+							+ (mina[i - 1][j] > mina[i - 1][j + 1] ? mina[i - 1][j] : mina[i - 1][j + 1]);
+
+					mina[i][j] = mayor;
+				} else if (i - 1 >= 0 && j == columna - 1) {
+					int mayor = mina[i][j]
+							+ (mina[i - 1][j] > mina[i - 1][j - 1] ? mina[i - 1][j] : mina[i - 1][j - 1]);
+					mina[i][j] = mayor;
+				}
+			}
+		}
+
+		// encuentro mayor de la ultima fila
+		res = mina[fila - 1][0];
+		for (int i = 1; i < columna; i++) {
+			if (mina[fila - 1][i] > res) {
+				res = mina[fila - 1][i];
+			}
+		}
+
 		return res;
 	}
 
@@ -70,13 +112,14 @@ public class Programacion_Dinamica {
 //		int plata = ladronDeCasas(vec);
 //
 //		System.out.println(plata);
-		
-		int[][] matriz= {{3,2,12,15,10},
-					{6,19,7,11,17},
-					{8,5,12,32,21},
-					{3,20,2,9,7}};
-		
-		System.out.println(minaDeOro(matriz));
+
+		int[][] matriz = { { 3, 2, 12, 15, 10 }, { 6, 19, 7, 11, 17 }, { 8, 5, 12, 32, 21 }, { 3, 20, 2, 9, 7 } };
+
+		System.out.println("MATRIZ ORIGINAL:");
+		imprimirMatriz(matriz);
+		System.out.println("MAXIMO ORO A SACAR: " + minaDeOro(matriz));
+		System.out.println("MATRIZ LUEGO DE LA MINA:");
+		imprimirMatriz(matriz);
 
 	}
 
